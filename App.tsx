2350679e77a82a71dea7dc394b34ff39c276a758
tontitor <ailtonarrudatec@gmail.com>
2022-10-22 17:17:10 -0300

    Creating button component

--- App.tsx
+++ App.tsx
@@ -4,18 +4,22 @@ import { StyleSheet, Switch, Text, View } from 'react-native';
 import { ThemeContext } from './src/context/ThemeContext'
 import { useState } from 'react';
 import { myColors } from './src/globalColors/Colors';
+import { Button } from './src/components/Button';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
   return (
     <ThemeContext.Provider value={theme}>
       <View style={theme === 'light' ? styles.container :[styles.container, {backgroundColor: myColors.dark}]}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text></Text>
         <StatusBar style="auto" />
         <Switch
           value={theme === 'light'}
           onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         />
+
+        <Button title='9' onPress={() => {}}/>
+
       </View>
     </ThemeContext.Provider>
    
